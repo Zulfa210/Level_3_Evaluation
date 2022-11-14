cf1790a64428fd6946d048b7e218b3b85c95d03b
Zulfa-Attar-Coditas <zulfaattar@gmail.com>
2022-11-14 09:50:08 +0530

    Added project structure for level 3 test

---
+++ src/main/java/com/example/Level_3_Evaluation/Level3EvaluationApplication.java
@@ -0,0 +1,13 @@
+package com.example.Level_3_Evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Level3EvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Level3EvaluationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Level_3_Evaluation/Level3EvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Level_3_Evaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Level3EvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
